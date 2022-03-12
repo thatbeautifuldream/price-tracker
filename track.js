20282c63acc3a3e35dcb417534075e80357857ee
Milind Mishra <milind.mishra4@gmail.com>
2022-03-13 04:13:59 +0530

    🐛 FIX: selectors update

--- track.js
+++ track.js
@@ -2,9 +2,13 @@ const puppeteer = require("puppeteer");
 const $ = require("cheerio").default;
 const CronJob = require("cron").CronJob;
 
-// .a-price.a-text-price.a-size-medium.apexPriceToPay > .a-offscreen
-const url =
-  "https://www.amazon.in/Logitech-Advanced-Wireless-Bluetooth-Keyboard/dp/B08WK11K94/";
+// #corePrice_desktop .a-price.a-text-price.a-size-medium.apexPriceToPay > .a-offscreen
+
+const url = process.argv[2]; // node track <url>
+// https://www.amazon.in/Logitech-Advanced-Wireless-Bluetooth-Keyboard/dp/B08WK11K94/
+
+const minPrice = process.argv[3]; // node track <url> <minPrice>
+// 19000
 
 async function configureBrowser() {
   const browser = await puppeteer.launch();
@@ -19,17 +23,17 @@ async function checkPrice(page) {
   // console.log(html);
 
   $(
-    ".a-price.a-text-price.a-size-medium.apexPriceToPay > .a-offscreen",
+    "#corePrice_desktop .a-price.a-text-price.a-size-medium.apexPriceToPay > .a-offscreen",
     html
   ).each(function () {
     let ruppeePrice = $(this).text();
     // console.log(ruppeePrice);
     let price = parseInt(ruppeePrice.replace("₹", "").replace(",", ""));
     console.log(price);
-    if (price > 19000) {
-      console.log("Price is greater than 19,000");
+    if (price > minPrice) {
+      console.log(`Price is greater than ${minPrice}`);
     } else {
-      console.log("Price is less than 19,000");
+      console.log(`Price is less than ${minPrice}`);
     }
   });
 }
