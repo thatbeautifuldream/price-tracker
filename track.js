f7981e2a70c19fa9992a9d301c66493fb925898b
Milind Mishra <milind.mishra4@gmail.com>
2022-03-13 13:37:01 +0530

    📦 NEW: added mailer

--- track.js
+++ track.js
@@ -1,6 +1,10 @@
+require("dotenv").config();
+
 const puppeteer = require("puppeteer");
 const $ = require("cheerio").default;
 const CronJob = require("cron").CronJob;
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 // Price CSS Selector : #corePrice_desktop .a-price.a-text-price.a-size-medium.apexPriceToPay > .a-offscreen
 
@@ -15,24 +19,45 @@ async function configureBrowser() {
 }
 
 async function checkPrice(page) {
-  await page.reload();
-  let html = await page.evaluate(() => document.body.innerHTML);
-  // console.log(html);
+  try {
+    await page.reload();
+    let html = await page.evaluate(() => document.body.innerHTML);
+    // console.log(html);
 
-  $(
-    "#corePrice_desktop .a-price.a-text-price.a-size-medium.apexPriceToPay > .a-offscreen",
-    html
-  ).each(function () {
-    let ruppeePrice = $(this).text();
-    // console.log(ruppeePrice);
-    let price = parseInt(ruppeePrice.replace("₹", "").replace(",", ""));
-    console.log(price);
-    if (price > minPrice) {
-      console.log(`Price is greater than ${minPrice}`);
-    } else {
-      console.log(`Price is less than ${minPrice}`);
-    }
-  });
+    $(
+      "#corePrice_desktop .a-price.a-text-price.a-size-medium.apexPriceToPay > .a-offscreen",
+      html
+    ).each(function () {
+      let ruppeePrice = $(this).text();
+      // console.log(ruppeePrice);
+      let price = parseInt(ruppeePrice.replace("₹", "").replace(",", ""));
+      console.log(price);
+      if (price < minPrice) {
+        console.log(`🛍 Price on ${url} is less than ${minPrice}`);
+        sendEmail(
+          `🛍 Price Is Low!`,
+          `Price on ${url} is less than ${minPrice}`
+        );
+      }
+    });
+  } catch (err) {
+    sendEmail(
+      `🛍 Price Check Failed`,
+      `Price Check Failed on ${url} with error ${err.message}`
+    );
+    throw err;
+  }
+}
+
+function sendEmail(subject, body) {
+  const email = {
+    to: "taxoha4598@tourcc.com",
+    from: "milind.mishra4@gmail.com",
+    subject: subject,
+    text: body,
+    html: body,
+  };
+  return sgMail.send(email);
 }
 
 async function startTracking() {
